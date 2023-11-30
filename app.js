99bd71cb12650ec83a9e237e9174634bad7a53f3
henry101g <66709823+henry101g@users.noreply.github.com>
2023-11-30 01:08:05 -0500

    Update app.js
    Connected Database

--- app.js
+++ app.js
@@ -3,32 +3,30 @@ const app = express();
 const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 const cors = require('cors');
-const items = require('./routes/api/items');
 
-//Connect Database
-app.use(cors({ origin: true, credentials: true }));
-app.use(express.json({extended: false}));
-app.get('/', (req, res) => res.send('Hello world!'));
-app.use('/api/items', items);
-app.get('/user/:id', (req, res) => {
-    res.send(`user ${req.params.id}`)
-});
+//const items = require('./routes/api/items');
+//app.use('/api/items', items);
 
-const conn_str = 'mongodb+srv://eschrack:7uxUwmeiuq6xXUZa@atlascluster.azhp7d6.mongodb.net/?retryWrites=true&w=majority'
+//connect database
+app.use(cors({origin: true, credentials: true }));
+app.use(express.json({ extended: false }));
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const conn_str = 'mongodb+srv://trackerWorkout:workoutTracker123@workouttracker.0hmr5dd.mongodb.net/?retryWrites=true&w=majority';
 mongoose.set('strictQuery', false);
-mongoose.connect(conn_str)
-.then(() => {
-    app.listen(port)
-    console.log(`MongoDB Connection Suceeded...`)
-    console.log(`Server running on port ${port}`)
+mongoose.connect(conn_str,{
+   useUnifiedTopology : true,
+   //useNewUrlParser : true
 })
-.catch(err => {
-    console.log(`Error in DB Connection ${err}`);
+.then(() => {
+   app.listen(port)
+console.log('MongoDB Connection Suceeded...');
+})
+.catch(err => { 
+   console.log('Error in DB connection ');
+   console.log( err);
 });
 
 
-
-
-
-// eschrack
-// 7uxUwmeiuq6xXUZa
+const items = require('./routes/api/items');
+app.use('/api/items', items);
