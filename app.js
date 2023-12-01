5c15d8b90c47add21c02b12583caf9c65875b413
Ethan Schrack <ethanschrack@gmail.com>
2023-12-01 15:12:07 -0500

    Added routes to app.js and tested all routes for all models (fully functional)

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//const items = require('./routes/api/items');
-//app.use('/api/items', items);
+const users = require('./routes/api/users');
+const exercises = require('./routes/api/exercises');
+const foods = require('./routes/api/foods');
 
 //connect database
 app.use(cors({origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('Hello World!'));
+// Routes for different models
+app.use('/api/users', users);
+app.use('/api/exercises', exercises);
+app.use('/api/foods', foods);
+
+app.get('/user/:id', (req, res) => {
+    res.send(`user ${req.params.id}`)
+});
 
 const conn_str = 'mongodb+srv://trackerWorkout:workoutTracker123@workouttracker.0hmr5dd.mongodb.net/?retryWrites=true&w=majority';
 mongoose.set('strictQuery', false);
@@ -21,12 +30,9 @@ mongoose.connect(conn_str,{
 .then(() => {
    app.listen(port)
 console.log('MongoDB Connection Suceeded...');
+console.log(`Server running on port ${port}`)
 })
 .catch(err => { 
    console.log('Error in DB connection ');
-   console.log( err);
+   console.log(err);
 });
-
-
-const items = require('./routes/api/items');
-app.use('/api/items', items);
